8ddd2f94254cf2b8b523c2036668d8ce9f915473
Your Real Name <your.email@example.com>
2026-03-19 14:16:53 +0530

    disable css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./style.css";
+// import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
